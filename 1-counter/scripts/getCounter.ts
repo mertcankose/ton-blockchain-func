b7840e79274e2285a8099e373fac3929b389f2eb
Mertcan Kose <mertcankose1106@gmail.com>
2025-02-19 10:25:55 +0300

    fix scripts & add provider process

--- 1-counter/scripts/getCounter.ts
+++ 1-counter/scripts/getCounter.ts
@@ -1,18 +1,13 @@
-import { getHttpEndpoint } from "@orbs-network/ton-access";
-import { TonClient, Address } from "@ton/ton";
-import {TestContract} from "../wrappers/TestContract"; // this is the interface class we just implemented
+import { Address } from "@ton/ton";
+import { TestContract } from "../wrappers/TestContract";
+import { NetworkProvider } from '@ton/blueprint';
 
-export async function run() {
-  // initialize ton rpc client on testnet
-  const endpoint = await getHttpEndpoint({ network: "testnet" });
-  const client = new TonClient({ endpoint });
-
+export async function run(provider: NetworkProvider) {
   // open Counter instance by address
-  const counterAddress = Address.parse("EQDOz4DsjrCmR4tE12LSVX44jeCXnytQ6SL4cDb-jS-xslch"); // replace with your address from step 8
-  const counter = new TestContract(counterAddress);
-  const counterContract = client.open(counter);
+  const counterAddress = Address.parse("EQDOz4DsjrCmR4tE12LSVX44jeCXnytQ6SL4cDb-jS-xslch"); // replace with your address
+  const counter = provider.open(TestContract.createFromAddress(counterAddress));
 
   // call the getter on chain
-  const counterValue = await counterContract.getCurrentValue();
+  const counterValue = await counter.getCurrentValue();
   console.log("value:", counterValue.toString());
 }
--- 1-counter/scripts/sendIncrement.ts
+++ 1-counter/scripts/sendIncrement.ts
@@ -1,61 +1,22 @@
-import { getHttpEndpoint } from "@orbs-network/ton-access";
-import { mnemonicToWalletKey } from "@ton/crypto";
-import { TonClient, WalletContractV4, Address, toNano, WalletContractV5R1 } from "@ton/ton";
+
+import { Address, toNano } from "@ton/ton";
 import { TestContract } from "../wrappers/TestContract";
-import * as dotenv from 'dotenv';
+import { NetworkProvider } from '@ton/blueprint';
 
-export async function run() {
-  // Load environment variables
-  dotenv.config();
-  
-  // initialize ton rpc client on testnet
-  const endpoint = await getHttpEndpoint({ network: "testnet" });
-  const client = new TonClient({ endpoint });
-
-  // Get mnemonic from environment variables
-  const mnemonic = process.env.MNEMONIC;
-  if (!mnemonic) {
-    throw new Error("MNEMONIC not found in environment variables");
-  }
-
-  const key = await mnemonicToWalletKey(mnemonic.split(" "));
-  const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
-  
-  // Add this line to see your wallet address
-  console.log("Wallet address:", wallet.address.toString());
-  
-  /*
-  if (!await client.isContractDeployed(wallet.address)) {
-    return console.log("wallet is not deployed");
-  }
-  */
-
-  // open wallet and read the current seqno of the wallet
-  const walletContract = client.open(wallet);
-  const walletSender = walletContract.sender(key.secretKey);
-  const seqno = await walletContract.getSeqno();
-
+export async function run(provider: NetworkProvider) {
   // open Counter instance by address
   const counterAddress = Address.parse("EQDOz4DsjrCmR4tE12LSVX44jeCXnytQ6SL4cDb-jS-xslch");
-  const counter = new TestContract(counterAddress);
-  const counterContract = client.open(counter);
+  const counter = provider.open(TestContract.createFromAddress(counterAddress));
 
   // send the increment transaction
-  await counterContract.sendIncreaseValue(walletSender, {
+  await counter.sendIncreaseValue(provider.sender(), {
     value: toNano('0.002'),
     increaseBy: 1
   });
 
-  // wait until confirmed
-  let currentSeqno = seqno;
-  while (currentSeqno == seqno) {
-    console.log("waiting for transaction to confirm...");
-    await sleep(1500);
-    currentSeqno = await walletContract.getSeqno();
-  }
+  // wait until transaction is confirmed
+  console.log("waiting for transaction to confirm...");
+  await provider.waitForDeploy(counter.address);
   console.log("transaction confirmed!");
 }
 
-function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
